783faec1843413ac57f70434ebc8d4934d689a4c
Jesse Giles <jesse.giles@telus.com>
2022-12-14 15:31:19 -0700

    created bootcamp_app folder and students.js, gitignore files

--- 4_queries/6_cohort_with_longest_asssitances.sql
+++ 4_queries/6_cohort_with_longest_asssitances.sql
@@ -3,5 +3,5 @@ FROM assistance_requests
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY average_assistance_time;
+ORDER BY average_assistance_time DESC
 LIMIT 1;
